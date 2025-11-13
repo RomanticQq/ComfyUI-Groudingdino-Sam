25670c95940c8e0a3acdd01d8ad56d4bc4ae5776
RomanticQq <3236997288@qq.com>
2025-11-13 18:22:40 +0800

    update

---
+++ groundingdino_draw_bbox.py
@@ -0,0 +1,81 @@
+import os
+import cv2
+import uuid
+import json
+import torch
+import requests
+import numpy as np
+from torchvision.ops import box_convert
+from datetime import datetime, timedelta
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+
+class GroundingDinoDrawBbox:
+    def __init__(self):
+        self.dir_name = os.path.dirname(__file__)
+        self.tmp_dir = os.path.join(os.path.dirname(__file__), "tmp")
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
+        self.model = load_model(f"{self.dir_name}/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", f"{self.dir_name}/GroundingDINO/weights/groundingdino_swint_ogc.pth")
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                # "image": ("IMAGE", {"default": None, "forceInput": True}),
+                "text_prompt": ("STRING", {"default": "subject"}),
+                "box_threshold": ("FLOAT", {"default": 0.35, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "text_threshold": ("FLOAT", {"default": 0.25, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "random_seed": ("INT", {"default": 66666, "min": 0, "max": 2**32 - 1, "step": 1, "control_after_generate": True}),
+            },
+            "optional": {
+                "image": ("IMAGE", {"default": None}),
+                "imageUrl": ("STRING", {"default": None}),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_TYPES_NAMES = ("image",)
+    FUNCTION = "test"
+    CATEGORY = "Grounding-Sam"
+    DESCRIPTION = "使用groundingdino进行目标检测，并绘制目标区域框"
+    def test(self, text_prompt="subject", box_threshold=0.35, text_threshold=0.25, random_seed=0, image=None, imageUrl=None):
+        np.random.seed(random_seed)
+        try:
+            tmp_img_name = str(uuid.uuid4()) + ".jpg"
+            tmp_img_path = os.path.join(self.tmp_dir, tmp_img_name)
+            if image is not None:
+                img = image.numpy()[0]
+                img = (img * 255).astype(np.uint8)
+                img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(tmp_img_path, img)
+            elif imageUrl is not None:
+                response = requests.get(imageUrl)
+                if response.status_code == 200:
+                    with open(tmp_img_path, 'wb') as f:
+                        f.write(response.content)
+                    print(f"Image downloaded from {imageUrl} and saved to {tmp_img_path}")
+                else:
+                    raise ValueError(f"Failed to download image from {imageUrl}, status code: {response.status_code}")
+            else:
+                raise ValueError("Either 'image' or 'imageUrl' must be provided.")
+            print(f"Processing image {tmp_img_path} with prompt '{text_prompt}'")
+            image_source, image = load_image(tmp_img_path)
+            boxes, logits, phrases = predict(
+                model=self.model,
+                image=image,
+                caption=text_prompt,
+                box_threshold=box_threshold,
+                text_threshold=text_threshold
+            )
+            if len(boxes) == 0:
+                print(f"No objects found in image {tmp_img_path}")
+            else:
+                annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
+                cv2.imwrite(tmp_img_path, annotated_frame)
+        except Exception as e:
+            print(f"Error processing image: {e}")
+        img = cv2.imread(tmp_img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = torch.from_numpy(np.expand_dims(img, axis=0) / 255.0)
+        os.remove(tmp_img_path)
+        # return (image,)
+        return (img,)
--- node.py
+++ node.py
@@ -1,6 +1,7 @@
 from .groundingdino_cut import GroundingDino
 from .grounded_sam2_cut_gaussian import GroundedSam2CutGaussian
 from .grounded_sam2_cut import GroundedSam2Cut
+from .groundingdino_draw_bbox import GroundingDinoDrawBbox
 
 
 # A dictionary that contains all nodes you want to export with their names
@@ -9,6 +10,7 @@ NODE_CLASS_MAPPINGS = {
     "groundingdino": GroundingDino,
     "grounded_sam2_cut_gaussian": GroundedSam2CutGaussian,
     "grounded_sam2_cut": GroundedSam2Cut,
+    "groundingdino_draw_bbox": GroundingDinoDrawBbox,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -16,4 +18,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "groundingdino": "GroundingDino",
     "grounded_sam2_cut_gaussian": "GroundedSam2CutGaussian",
     "grounded_sam2_cut": "GroundedSam2Cut",
+    "groundingdino_draw_bbox": "GroundingDinoDrawBbox",
 }
